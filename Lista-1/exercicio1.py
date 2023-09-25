f077f80d41c2b3f5fddc3d88d1963f8f3a21e79c
PedroPureza <pedroquadradopureza@gmail.com>
2023-09-25 20:24:47 -0300

    exercicio 1

---
+++ Lista-1/exercicio1.py
@@ -0,0 +1,20 @@
+# Adicione 2 atributos e 2 métodos à classe Mago.
+
+#obs: o exemplo utilizado em aula continha 4 atributos: Nome,Poder,Idade e EscolaMagia
+# e 3 métodos: Andar(), Falar(), InvocarMagia()
+
+class Mago:
+  def __init__(self, nivel, manaMaxima):
+    self.nivel = nivel
+    self.manaMaxima = manaMaxima
+    
+  def upar_nivel(self):
+    self.nivel = self.nivel + 1
+    self.manaMaxima = self.manaMaxima + (self.nivel * 0.3)
+    print(f'Parabéns! Agora você é nível {self.nivel}')
+
+  def checar_nivel(self):
+    print(f'Você está no nível {self.nivel}')
+
+mago = Mago(2, 120)
+mago.checar_nivel()
